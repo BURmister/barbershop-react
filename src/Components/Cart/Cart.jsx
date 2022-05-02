8d3981aa278c76da0d623297c84daf9c7e290f4b
BURmister <olegcernysov903@gmail.com>
2022-05-03 00:32:50 +0300

    changes in Cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -11,16 +11,25 @@ function Cart(props) {
             <div className="drawer__wrapper">
                 <div onClick={props.clickOnClose} className="drawer__shadow" ></div>
                 <div className="drawer"> 
-                    <div className="drawer__container">
-                        <div className="drawer__content__container">
-                            <h2 className="drawer__h2">это твоя корзина</h2>
+                    <div className="drawer__flex">
+                        
+                        <div className="drawer__header">
+                            <h2 className="drawer__h2">это ваша корзина</h2>
                             <div onClick={props.clickOnClose} className="drawer__close"><img className="drawer__close__img" width="20" src={iconClear}></img></div>
                         </div>
+
                         <div className="drawer__content">
-                            <ShopCard/>
+                            <div className="content__wrapper">
+                                <ShopCard/>
+                                <ShopCard/>
+                                <ShopCard/>
+                            </div>
                         </div>
-                        <div className="drawer__buy">
-                            <div className="drawer__button__buy">это кнопка</div>
+                        <div className="drawer__container__footer">
+                            <div className="drawer__footer">
+                                <div>итого к оплате: </div>
+                                <button className=" button drawer__button__buy">оформление заказа</button>
+                            </div>
                         </div>
                     </div>
                 </div>
